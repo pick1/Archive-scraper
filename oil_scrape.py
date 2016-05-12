30869825a0a46d9d07c20385d0988bf16311b540
pick1 <pick1@users.noreply.github.com>
2016-05-12 09:38:45 -0700

    updated for cleaner reading and entry

--- oil_scrape.py
+++ oil_scrape.py
@@ -12,24 +12,24 @@ mydb = MySQLdb.connect(host='localhost',
    db='****')
 cur = mydb.cursor()
 # setup MySQL table for storage
-def store (headline):
-    cur.execute('INSERT IGNORE INTO OILNEWS (headline) VALUES ( \"%s\")',(headline, ))
+def store (auth_date, url):
+    cur.execute('INSERT IGNORE INTO OILNEWS (auth_date, url) VALUES ( \"%s\", \"%s\")',(auth_date, url ))
     cur.connection.commit()
 base_url = "http://oilprice.com/Latest-Energy-News/World-News"
-
-# page request; page 1 through n
-n = 376
+# request page 1 through n; where 1 = max. pages to scrape
+n = 1  
 for i in range(1, n+1):
     if (i == 1):
     # handle first page
         response = urlopen(base_url)
     response = urlopen(base_url + "/Page-%d.html" % i)
     html = response
-
-    # data parsing
     soup = BeautifulSoup(html.read().decode('utf-8'),"lxml")
-    table = soup.find('div', {'class':'tableGrid__column tableGrid__column--articleContent category'})
-    for a in table.find_all('a', href=True)[::2][:-5]:
-        headline = a['href']
-        store(headline)
-        print headline
+    main_div = soup.select_one("div.tableGrid__column.tableGrid__column--articleContent.category")
+    divs = main_div.select('div.categoryArticle__content')
+    for d in divs:
+        auth_date = d.select_one("p.categoryArticle__meta").text
+        url = d.a["href"]
+        store (auth_date, url)
+        print (auth_date, url)
+        print '\n'
