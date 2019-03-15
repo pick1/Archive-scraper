9017ba69bebd23d7ac4a9ad215ce1f6951eb845d
Derek_Pickern <dp@serverbeast.attlocal.net>
2019-03-15 10:59:17 -0700

    Updated FDA scrape to be more robust

--- fda_scrape.py
+++ fda_scrape.py
@@ -1,18 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-import MySQLdb
+import pandas as pd
+import numpy as np
+from datetime import datetime as dt
 
-#mysql portion
-mydb = MySQLdb.connect(host='localhost',
-       user= 'user',
-       passwd='password',
-       db='dbname')
-cur = mydb.cursor()
-def store (drug_name, active_ingredient, dosage_form_route, sponsor, submission_type):
-        cur.execute('INSERT IGNORE INTO FDA (drug_name, active_ingredient, dosage_form_route, sponsor, submission_type) VALUES (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\")',(drug_name, active_ingredient, dosage_form_route, sponsor, submission_type))
-        cur.connection.commit()
 
-base_url = 'https://www.accessdata.fda.gov/scripts/cder/daf/index.cfm?event=report.page'
+today = dt.now().strftime('%Y-%m-%d')
+
+links = []
+drugname = []
+strength = []
+ingreds = []
+mstat = []
+doses = []
+subs = []
+cos = []
+subc = []
+substat = []
+
+base_url = 'https://www.accessdata.fda.gov/scripts/cder/daf/index.'\
+           'cfm?event=report.page'
+
+base_link_url = 'https://www.accessdata.fda.gov/scripts/cder/daf/index.'\
+                'cfm?event=overview.process&ApplNo='
+
 html = requests.get(base_url)
 soup = BeautifulSoup(html.content, "html.parser")
 
@@ -21,21 +32,31 @@ rows = table[0].findAll("tr")
 if len(soup.findAll('th')) > 0:
         rows = rows[1:]
 for row in rows:
-        cells = row.findAll('td')
-        drug_name = cells[0].get_text()
-        active_ingredient =  cells[1].get_text()
-        dosage_form_route = cells[2].get_text()
-        sponsor = cells[3].get_text()
-        submission_type = cells[4].get_text()
-        store(drug_name, active_ingredient, dosage_form_route, sponsor, submission_type)
-        data = {
-           'drug_name': cells[0].get_text(),
-           'active_ingredient': cells[1].get_text(),
-           'dosage_form_route': cells[2].get_text(),
-           'sponsor': cells[3].get_text(),
-           'submission_type': cells[4].get_text(),
-        }
+    link = base_link_url + str(row.td.br.text).split('#')[1]
+    links.append(link)
+    linkhtml = requests.get(link)
+    linksoup = BeautifulSoup(linkhtml.text, "lxml")
+    linkrow = linksoup.findAll('tbody')[0]
+    linktds = linkrow.findAll('td')
+    drugname.append(linktds[0].text)
+    strength.append(linktds[2].text)
+    mstat.append(linktds[4].text)
+    tds = row.findAll('td')
+    ingreds.append(tds[1].get_text())
+    doses.append(tds[2].get_text())
+    subs.append(tds[3].get_text())
+    cos.append(tds[4].get_text())
+    subc.append(tds[5].get_text())
+    if len(str(subc)) > 1:
+        subc.append(subc)
+    else:
+        subc.append(np.nan)
+    substat.append(tds[6].get_text())
 
-        print data
-        print '\n'
-mydb.close()
+df = pd.DataFrame(list(zip(cos, drugname, subs, substat, mstat,
+                           links, ingreds, doses, subc, strength)),
+                  columns=['company', 'drugname', 'submission',
+                           'submission_status', 'marketing_status',
+                           'link', 'ingredients', 'dosage',
+                           'submission_class', 'strength'])
+df['date'] = pd.to_datetime(today)
